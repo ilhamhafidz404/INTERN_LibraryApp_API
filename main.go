f1b7a4c9bfeaa91a1bc41a100b68607d795f9071
ilhamhafidz404 <xxspanzie@egmail.com>
2025-06-29 19:28:10 +0700

    Model Migration User & Book

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"library_app/database"
+	"library_app/models"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
     app := fiber.New()
+
+    // migration
+    database.Connect()
+    database.DB.AutoMigrate(
+        models.Book{},
+        &models.User{},
+    )
 
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, Fiber!")
---
+++ models/Book.go
@@ -0,0 +1,14 @@
+package models
+
+import "gorm.io/gorm"
+
+type Book struct {
+    gorm.Model
+    Code        string 	`gorm:"type:varchar(7);unique;not null" json:"code"`          
+    Title       string 	`gorm:"type:varchar(100);unique;not null" json:"title"`          
+    Publisher   string 	`gorm:"type:varchar(50);not null" json:"publisher"`               
+    Author 		string 	`gorm:"type:varchar(50);not null" json:"author"`      
+    ISBN  		string 	`gorm:"type:varchar(13);not null" json:"isbn"`         
+    Year 		uint16 	`gorm:"not null" json:"year"`          
+    Total       int64 	`gorm:"not null" json:"total"`          
+}
